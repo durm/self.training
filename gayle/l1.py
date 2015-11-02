6801569e774dccccbfe6a02b17f854517b3bb060
Alex Korotkov <durm.icecoffee@gmail.com>
2015-11-02 21:10:32 +0300

    level 1 complete
    first part finished

--- gayle/l1.py
+++ gayle/l1.py
@@ -21,6 +21,54 @@ def t4(s):
     return s.replace(" ", "%20")
     
     
+def t5(s):
+    curr = None
+    n = 0
+    result = ""
+    for c in s:
+        if curr == c:
+            n += 1
+        else:
+            if curr is not None:
+                result += curr + str(n)
+            curr = c
+            n = 1
+    result += curr + str(n)
+    return result if len(result) < len(s) else s
+
+    
+def t6(src):
+    import copy
+    trg = copy.deepcopy(src)
+    ln = len(trg)
+    trg = [[trg[j][i] for j in range(ln)] for i in range(ln)]
+    trg = list(map(lambda l: list(reversed(l)), trg))
+    return trg
+    
+    
+def t7(src):
+    trg = src.copy()
+    nullrows = []
+    nullcols = []
+    for i, e in enumerate(src):
+        if 0 in e:
+            nullrows.append(i)
+            for j, e2 in enumerate(e):
+                if e2 == 0:
+                    nullcols.append(j)
+    return [[(0 if i in nullrows or j in nullcols else col) for j, col in enumerate(row)] for i, row in enumerate(src)]
+    
+    
+def t8(src, trg):
+    ln = len(src)
+    if ln != len(trg):
+        return False
+    for i in range(1, ln-1):
+        if src[i:] + src[:i] == trg:
+            return True
+    return False
+
+    
 class Tests(unittest.TestCase):
     
     
@@ -42,5 +90,21 @@ class Tests(unittest.TestCase):
         self.assertEqual(t4("a b"), "a%20b")
 
         
+    def test_t5(self):
+        self.assertEqual(t5("aaabbnnnnn"), "a3b2n5")
+        
+        
+    def test_t6(self):
+        self.assertEqual(t6([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [[7, 4, 1], [8, 5, 2], [9, 6, 3]])
+        
+        
+    def test_t7(self):
+        self.assertEqual(t7([[1, 2, 3], [4, 0, 6], [7, 8, 9]]), [[1, 0, 3], [0, 0, 0], [7, 0, 9]])
+    
+    
+    def test_t8(self):
+        self.assertTrue(t8("asb", "sba"))
+    
+    
 if __name__ == "__main__":
     unittest.main()
